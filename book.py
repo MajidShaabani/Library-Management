9d459de05738bd1a590d5d6db6139c648f13900a
MajidShaabani <majid.shaabani755@gmail.com>
2025-04-17 17:57:49 +0200

    book model

---
+++ book.py
@@ -0,0 +1,33 @@
+class Book:
+    def __init__(self, book_id, title, author, isbn, available=True):
+        self.book_id = book_id
+        self.title = title
+        self.author = author
+        self.isbn = isbn
+        self.available = available
+    
+    def display_info(self):
+        status = "Available" if self.available else "Not Available"
+        return f"ID: {self.book_id}, Title: {self.title}, Author: {self.author}, ISBN: {self.isbn}, Status: {status}"
+    
+    def update_availability(self, is_available):
+        self.available = is_available
+        
+    def to_dict(self):
+        return {
+            "book_id": self.book_id,
+            "title": self.title,
+            "author": self.author,
+            "isbn": self.isbn,
+            "available": self.available
+        }
+    
+    @classmethod
+    def from_dict(cls, data):
+        return cls(
+            book_id=data["book_id"],
+            title=data["title"],
+            author=data["author"],
+            isbn=data["isbn"],
+            available=data["available"]
+        )
